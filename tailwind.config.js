c858fe34fd962fac1d1454a50ed4b8390e024dc8
GrimReaper606 <71063135+GrimReaper606@users.noreply.github.com>
2022-11-01 12:35:03 +0600

    Nav Bar created working on hamburger for mobile

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors : {
+        brightBlue: '#5EA2B7',
+        brightGray:  '#D9D9D9',
+      }
+    },
+  },
+  plugins: [],
+}
+
