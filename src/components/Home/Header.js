29dd529796c02437cb34844e0ebd6e853ad14f53
Faerque <202001512@eastdelta.edu.bd>
2022-12-20 09:59:16 +0600

    All done before viva

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className='m-3'>
-            <section className="mb-20">
-                <div className="relative overflow-hidden rounded-md bg-no-repeat bg-cover" style={{ backgroundPosition: "50%", backgroundImage: `URL('https://thumbs.dreamstime.com/z/farmer-works-cabbage-field-dumuria-khulna-bangladesh-128219525.jpg')`, height: "500px" }}></div>
+            <section className="mt-48">
                 <div className="container mx-auto px-6 md:px-12 xl:px-32">
                     <div className="text-center text-gray-800">
                         <div className="block rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12" style={{ marginTop: "-170px", background: "hsla(0, 0%, 100%, 0.7)", backdropFilter: "blur(30px)" }}>
